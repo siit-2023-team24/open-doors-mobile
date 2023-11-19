5f383e336529541412800cc4afdeb986f449c923
Sonja-Parabucki <105170152+Sonja-Parabucki@users.noreply.github.com>
2023-11-19 15:01:10 +0100

    minor fixes

--- app/src/main/java/com/bookingapptim24/activities/EditAccountActivity.java
+++ app/src/main/java/com/bookingapptim24/activities/EditAccountActivity.java
@@ -4,17 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 
 import com.bookingapptim24.HomeScreen;
 import com.bookingapptim24.R;
+import com.bookingapptim24.databinding.ActivityEditAccountBinding;
 
 public class EditAccountActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_edit_account);
+        ActivityEditAccountBinding binding = ActivityEditAccountBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         Button saveBtn = findViewById(R.id.save_btn);
         saveBtn.setOnClickListener((v) -> {
             Intent intent = new Intent(EditAccountActivity.this, HomeScreen.class);
@@ -23,5 +26,12 @@ public class EditAccountActivity extends AppCompatActivity {
 
             //how to open the profile fragment instead od home page?
         });
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_item,
+                getResources().getStringArray(R.array.countries));
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        binding.countrySpinner.setAdapter(adapter);
     }
 }
