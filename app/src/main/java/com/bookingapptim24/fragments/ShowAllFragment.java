0e87aa54588f00bd2f4561d49b3cebd2d4c8b040
milicamisic <milicamisic2303@gmail.com>
2023-11-19 12:15:50 +0100

    [ADD] On click listeners for search and filter buttons

--- app/src/main/java/com/bookingapptim24/fragments/ShowAllFragment.java
+++ app/src/main/java/com/bookingapptim24/fragments/ShowAllFragment.java
@@ -1,14 +1,21 @@
 package com.bookingapptim24.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.bookingapptim24.FilterActivity;
+import com.bookingapptim24.LoginScreen;
 import com.bookingapptim24.R;
+import com.bookingapptim24.RegisterScreen;
+import com.bookingapptim24.SearchActivity;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -58,9 +65,27 @@ public class ShowAllFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_show_all, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_show_all, container, false);
+
+        Button searchButton = view.findViewById(R.id.searchButton);
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(requireActivity(), SearchActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button filterButton = view.findViewById(R.id.filterButton);
+        filterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(requireActivity(), FilterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        return view;
     }
 }
